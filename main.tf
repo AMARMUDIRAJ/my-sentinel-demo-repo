295a04361be70b4b7b9fbfe47a1f7cde0921371a
Amar Mudiraj <amarmudiraj@gmail.com>
2024-04-18 11:12:24 +0530

    add demo file for template-self link for compute instance with template resource

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "google_compute_instance_from_template" "tpl" {
   name = "instance-from-template"
   zone = "us-central1-a"
 
-  source_instance_template = google_compute_instance_template.example-template.self_link_unique
+  source_instance_template = ""https://www.googleapis.com/compute/v1/projects/my-dev-project-399904/global/instanceTemplates/example-template""
 
   // Override fields from instance template
   can_ip_forward = false
