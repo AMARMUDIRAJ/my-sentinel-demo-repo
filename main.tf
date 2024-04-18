56485faf06318b734a0027bb2c01f6f5780cf33a
Amar Mudiraj <amarmudiraj@gmail.com>
2024-04-18 11:44:46 +0530

    add demo file for template-self link for compute instance with template selflink

--- main.tf
+++ main.tf
@@ -2,33 +2,12 @@ provider "google" {
   project = "my-dev-project-399904"
   region  = "us-central1"
 }
-resource "google_compute_instance_template" "tpl" {
-  name         = "template"
-  machine_type = "e2-medium"
-
-  disk {
-    source_image = "debian-cloud/debian-11"
-    auto_delete  = true
-    disk_size_gb = 100
-    boot         = true
-  }
-
-  network_interface {
-    network = "default"
-  }
-
-  metadata = {
-    foo = "bar"
-  }
-
-  can_ip_forward = true
-}
 
 resource "google_compute_instance_from_template" "tpl" {
   name = "instance-from-template"
   zone = "us-central1-a"
 
-  source_instance_template = google_compute_instance_template.tpl.self_link_unique
+  source_instance_template = google_compute_instance_template.example-template.self_link
 
   // Override fields from instance template
   can_ip_forward = false
