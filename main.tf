7112df03a8dc86b915813c2ea14df87e2aa661eb
Amar Mudiraj <amarmudiraj@gmail.com>
2024-04-18 10:58:43 +0530

    add demo files

--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
-resource "google_storage_bucket" "static-site" {
-  name          = "image-store.com-2024-04-my-dev-project-399904"
-  location      = "EU"
-  force_destroy = true
+provider "google" {
+  project = "my-dev-project-399904"
+  region  = "yus-central1"
+}
 
-  uniform_bucket_level_access = true
+resource "google_compute_instance_template" "example" {
+  name        = "example-template"
+  machine_type = "n1-standard-1"
+  disk {
+    source_image = "debian-cloud/debian-10"
+    auto_delete  = true
+  }
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
 
-
+output "template_self_link" {
+  value = google_compute_instance_template.example.self_link
 }
