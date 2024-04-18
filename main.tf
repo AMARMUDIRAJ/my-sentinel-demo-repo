298681d12d52c6a9cd9ac00c37b31e76c1d295f9
AMARMUDIRAJ <69884578+AMARMUDIRAJ@users.noreply.github.com>
2024-04-18 12:15:21 +0530

    Update main.tf---google_compute_instance_template.example.self_link

--- main.tf
+++ main.tf
@@ -7,8 +7,7 @@ resource "google_compute_instance_from_template" "tpl" {
   name = "instance-from-template"
   zone = "us-central1-a"
 
-  source_instance_template = google_compute_instance_template.example-template
-
+  source_instance_template = google_compute_instance_template.example.self_link
   // Override fields from instance template
   can_ip_forward = false
   labels = {
