fa7d09d109745443652b16a4d9a2c8f17cf1a292
AMARMUDIRAJ <69884578+AMARMUDIRAJ@users.noreply.github.com>
2024-04-25 20:47:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,17 +20,17 @@ output "template_self_link" {
   value = google_compute_instance_template.example.self_link
 }
 
-resource "google_compute_instance_from_template" "tpl" {
-  name = "instance-from-template"
-  zone = "us-central1-a"
+#resource "google_compute_instance_from_template" "tpl" {
+#  name = "instance-from-template"
+#  zone = "us-central1-a"
 
-  source_instance_template = google_compute_instance_template.example.self_link
+#  source_instance_template = google_compute_instance_template.example.self_link
 
-  // Override fields from instance template
-  can_ip_forward = false
-  labels = {
-    my_key = "my_value"
-  }
-}
+#  // Override fields from instance template
+#  can_ip_forward = false
+#  labels = {
+#   my_key = "my_value"
+#  }
+#}
 
 ######################
