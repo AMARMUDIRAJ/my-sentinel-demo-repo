80616f6fec2b13e944bed04d3109e4f946e67d16
AMARMUDIRAJ <69884578+AMARMUDIRAJ@users.noreply.github.com>
2024-04-29 10:16:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ provider "google" {
   region  = "us-central1"
 }
 
-data "google_compute_instance_template" "existing_template" {
+data "google_compute_instance_template" "example-template12345" {
   self_link = "projects/my-dev-project-399904/global/instancetemplates/example-template12345"
 }
 
 resource "google_compute_instance_from_template" "instance" {
   name           = "instance-from-template"
   zone           = "us-central1-a"
-  source_instance_template = data.google_compute_instance_template.existing_template.self_link
+  source_instance_template = data.google_compute_instance_template.example-template12345.self_link
 }
