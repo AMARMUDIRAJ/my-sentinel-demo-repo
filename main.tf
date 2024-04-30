c3a601d1fce28944ee873427e728b54a299f21f5
AMARMUDIRAJ <69884578+AMARMUDIRAJ@users.noreply.github.com>
2024-04-30 19:45:02 +0530

    Update main.tf---provisioning compute_region_disk with snapshot and disk resource

--- main.tf
+++ main.tf
@@ -1,14 +1,28 @@
-data "google_compute_instance_template" "example-template12345" {
-  name = "example-template12345"
-}
-
 provider "google" {
   project = "my-dev-project-399904"
   region  = "us-central1"
 }
 
-resource "google_compute_instance_from_template" "instance" {
-  name                        = "instance-from-template"
-  zone                        = "us-central1-a"
-  source_instance_template    = data.google_compute_instance_template.example-template12345.self_link
+resource "google_compute_region_disk" "regiondisk" {
+  name                      = "my-region-disk"
+  snapshot                  = google_compute_snapshot.snapdisk.id
+  type                      = "pd-ssd"
+  region                    = "us-central1"
+  physical_block_size_bytes = 4096
+
+  replica_zones = ["us-central1-a", "us-central1-f"]
+}
+
+resource "google_compute_disk" "disk" {
+  name  = "my-disk"
+  image = "debian-cloud/debian-11"
+  size  = 50
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+}
+
+resource "google_compute_snapshot" "snapdisk" {
+  name        = "my-snapshot"
+  source_disk = google_compute_disk.disk.name
+  zone        = "us-central1-a"
 }
